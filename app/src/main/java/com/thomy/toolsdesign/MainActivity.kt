a771b8d6829704ce91c22ac8bee711395be9dcc4
thomychancamedina <(Rocky20!*)>
2020-11-19 23:05:05 +0100

    change dimension for time circle

--- app/src/main/java/com/thomy/toolsdesign/MainActivity.kt
+++ app/src/main/java/com/thomy/toolsdesign/MainActivity.kt
@@ -2,10 +2,27 @@ package com.thomy.toolsdesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.thomy.library.CountDownTime
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var countDownTimerView: CountDownTime
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        countDownTimerView = this.findViewById(R.id.countDownTimerView)
+
+
+        countDownTimerView.startTimer(
+                timerCount = 30,
+                onCountDownTimerStarted = {
+                    Log.d("Timer: ", "Started")
+                },
+                onCountDownTimerRunning = {
+                    Log.d("Timer: ", "Running $it")
+                },
+                onCountDownTimerStopped = {
+                    Log.d("Timer: ", "Stopped")
+                })
     }
 }
